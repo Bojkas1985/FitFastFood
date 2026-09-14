d4631d75864b6c98c7c4adbb91568a3ffd5b6e79
Bojkas1985 <41238303+Bojkas1985@users.noreply.github.com>
2026-09-14 20:07:14 +0200

    Add subwayData to FOOD_DATA export

--- data/index.ts
+++ data/index.ts
@@ -1,10 +1,12 @@
 import { kfcData } from './kfc';
 import { mcdonaldsData } from './mcdonalds';
 import { burgerkingData } from './burgerking';
+import { subwayData } from './subway';
 import { FoodItem } from '../types/food';
 
 export const FOOD_DATA: FoodItem[] = [
   ...mcdonaldsData,
   ...kfcData,
   ...burgerkingData,
+  ...subwayData,
 ];
