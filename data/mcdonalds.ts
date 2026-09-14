1a8aa340105a68b726737f61605c8fd28eea969f
Bojkas1985 <41238303+Bojkas1985@users.noreply.github.com>
2026-09-14 18:43:38 +0200

    Create mcdonalds.ts with menu item data
    Added data for McDonald's menu items including nutritional information.

---
+++ data/mcdonalds.ts
@@ -0,0 +1,9 @@
+import { FoodItem } from '../types/food';
+
+export const mcdonaldsData: FoodItem[] = [
+  { id: 'mc-1', chain: 'mcdonalds', name: 'Double Cheeseburger', calories: 440, protein: 26, carbs: 35, fat: 23, tip: 'TOP poměr cena / protein v Mekáči' },
+  { id: 'mc-2', chain: 'mcdonalds', name: 'Chicken McNuggets (9ks)', calories: 390, protein: 23, carbs: 28, fat: 20, tip: 'Dej si hořčici nebo kečup místo majonézy' },
+  { id: 'mc-3', chain: 'mcdonalds', name: 'McChicken', calories: 420, protein: 20, carbs: 41, fat: 19, tip: 'V kiosku odeber majonézu = -100 kcal tuku' },
+  { id: 'mc-4', chain: 'mcdonalds', name: 'McRoyal', calories: 520, protein: 31, carbs: 37, fat: 27 },
+  { id: 'mc-5', chain: 'mcdonalds', name: 'McWrap Křupavé Kuře', calories: 580, protein: 24, carbs: 54, fat: 29 },
+];
