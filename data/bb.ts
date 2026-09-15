115c2a6e7fa18bc1533e9b3a4633d0d2e5a57880
Bojkas1985 <41238303+Bojkas1985@users.noreply.github.com>
2026-09-15 09:20:01 +0200

    Create bb.ts

---
+++ data/bb.ts
@@ -0,0 +1,12 @@
+import { FoodItem } from '../types/food';
+
+export const bbData: FoodItem[] = [
+  { id: 'bb-caesar', chain: 'bb', name: 'Caesar bageta', calories: 487, protein: 27.3, carbs: 46.7, fat: 21.6, tip: 'Skvělý zdroj kuřecího proteinu', isVegetarian: false },
+  { id: 'bb-caprese', chain: 'bb', name: 'Caprese bageta', calories: 496, protein: 20.5, carbs: 48.7, fat: 24.6, isVegetarian: true },
+  { id: 'bb-lososova', chain: 'bb', name: 'Lososová bageta', calories: 366, protein: 20.3, carbs: 45.6, fat: 15.6, tip: 'Nejnižší kalorie z celých baget (366 kcal)', isVegetarian: false },
+  { id: 'bb-tunakova', chain: 'bb', name: 'Tuňáková bageta', calories: 446, protein: 22.8, carbs: 45.9, fat: 18.9, isVegetarian: false },
+  { id: 'bb-bruselska', chain: 'bb', name: 'Bruselská bageta', calories: 493, protein: 29.1, carbs: 52.2, fat: 20, tip: 'Vysoká porce 29g bílkovin', isVegetarian: false },
+  { id: 'bb-parizska', chain: 'bb', name: 'Pařížská bageta', calories: 604, protein: 25.1, carbs: 53.6, fat: 32.1, isVegetarian: false },
+  { id: 'bb-sweet-ribs', chain: 'bb', name: 'Sweet Ribs bageta', calories: 580, protein: 29.9, carbs: 64.5, fat: 22, tip: 'Téměř 30g proteinu z vepřových žeber', isVegetarian: false },
+  { id: 'bb-svycarska', chain: 'bb', name: 'Švýcarská bageta', calories: 520, protein: 27.6, carbs: 54.6, fat: 41.6, tip: 'Sýrová proteinová bomba pro vegetariány', isVegetarian: true },
+];
